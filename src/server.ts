f3668b2f33ce7168962027588fce434ba215ec7d
luizevrs <luiizevrsj@gmail.com>
2025-08-21 11:46:39 -0300

    feat: add basic routing with /ping endpoint and global error handler

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
-import express from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 import cors from "cors";
+import { routes } from "./routes/main.js";
 
 const server = express();
 server.use(cors());
 server.use(express.static("public"));
 server.use(express.json());
+
+server.use(routes);
+server.use(
+  (error: any, request: Request, response: Response, next: NextFunction) => {
+    console.error(error);
+    response.status(500).json({ error: "Ops, Parece que algo deu errado" });
+  }
+);
+server.listen(4000, () => {
+  console.log("running");
+});
